70ca871748b74872359f191c30a077b04ee5120b
Yun_Hydrogen <Yun_Hydrogen@outlook.com>
2025-02-03 23:24:47 +0800

    Finish FormsPage!

--- FormsPage.py
+++ FormsPage.py
@@ -1,7 +1,36 @@
 from PyQt5.QtWidgets import QWidget
 from  Ui_FormsPage import  Ui_FormsPage
+from qfluentwidgets import FluentIcon
+from PyQt5.QtGui import QPixmap, QDesktopServices
+from PyQt5.QtCore import QUrl
+from qfluentwidgets import MessageBox
 
 class FormsPage(QWidget, Ui_FormsPage):
     def __init__(self,parent=None):
         super().__init__(parent=parent)
         self.setupUi(self)
+        
+        #设置图标
+        self.PassPortButton.setIcon(FluentIcon.LINK)
+        self.GithubButton.setIcon(FluentIcon.GITHUB)
+        self.GitHub_Gird.setIcon(FluentIcon.GITHUB)
+        self.MoreInfoButton.setIcon(FluentIcon.LINK)
+        self.ReportLinkButton.setIcon(FluentIcon.LINK)
+
+        #设置图片
+        self.QRcode_PassPort.setPixmap(QPixmap("src/PassPort-Application-Qrcode.png"))
+        self.MoreInfo_QRcode.setPixmap(QPixmap("src/MoreInfoQRcode.png"))
+
+        #链接函数
+        self.ReportLinkButton.clicked.connect(self.ReportLinkButton_Clicked)
+    
+    #链接执行
+    def ReportLinkButton_Clicked(self):
+        #设置对话框
+        MB = MessageBox("注意ψ(._. )>", "你正在跳转到第三方网页以进行进一步操作，是否继续？", self.window())
+        MB.yesButton.setText("继续")
+        MB.cancelButton.setText("取消")
+        if MB.exec():
+            QDesktopServices.openUrl(QUrl("https://docs.qq.com/form/page/DTWVTUmxpZkVveWJN"))
+        else:
+            pass
--- Main.py
+++ Main.py
@@ -1,20 +1,20 @@
 import sys,qfluentwidgets
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication
-from qfluentwidgets import FluentIcon, SplitFluentWindow
+from qfluentwidgets import FluentIcon, MSFluentWindow
 from  PyQt5.QtCore import Qt
 from FormsPage import FormsPage
 
-class DLLauncher(SplitFluentWindow):
+class DLLauncher(MSFluentWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("月梦初晓MC 启动器")
         self.setWindowIcon(QIcon("src/ICO.png"))
         qfluentwidgets.setThemeColor("#66ccff")
-        
+        self.setFixedSize(920, 540)
         #添加子页面
         self.FormsPage = FormsPage(self)
-        self.addSubInterface(self.FormsPage, FluentIcon.FEEDBACK, "表单中心")
+        self.addSubInterface(self.FormsPage, FluentIcon.LABEL, "表单")
 
 
 if __name__ == "__main__":
