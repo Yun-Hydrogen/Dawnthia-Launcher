6d9279c65625fc0d81d076386f8e226ec5361e6c
Yun_Hydrogen <Yun_Hydrogen@outlook.com>
2025-02-03 23:39:13 +0800

    True : Finished FormsPage

--- FormsPage.py
+++ FormsPage.py
@@ -23,14 +23,43 @@ class FormsPage(QWidget, Ui_FormsPage):
 
         #链接函数
         self.ReportLinkButton.clicked.connect(self.ReportLinkButton_Clicked)
+        self.GithubButton.clicked.connect(self.GithubButton_Clicked)
+        self.PassPortButton.clicked.connect(self.PassPortButton_Clicked)
+        self.MoreInfoButton.clicked.connect(self.MoreInfoButton_Clicked)
     
     #链接执行
-    def ReportLinkButton_Clicked(self):
-        #设置对话框
+    def ReportLinkButton_Clicked(self):#反馈按钮
         MB = MessageBox("注意ψ(._. )>", "你正在跳转到第三方网页以进行进一步操作，是否继续？", self.window())
         MB.yesButton.setText("继续")
         MB.cancelButton.setText("取消")
         if MB.exec():
             QDesktopServices.openUrl(QUrl("https://docs.qq.com/form/page/DTWVTUmxpZkVveWJN"))
+        else:
+            pass
+    
+    def GithubButton_Clicked(self):#GitHub按钮
+        MB = MessageBox("注意ψ(._. )>", "你正在跳转到第三方网页以进行进一步操作，是否继续？", self.window())
+        MB.yesButton.setText("继续")
+        MB.cancelButton.setText("取消")
+        if MB.exec():
+            QDesktopServices.openUrl(QUrl("https://github.com/Yun-Hydrogen/DreamingLight-Launcher"))
+        else:
+            pass
+    
+    def PassPortButton_Clicked(self):#通行证按钮
+        MB = MessageBox("注意ψ(._. )>", "你正在跳转到第三方网页以进行进一步操作，是否继续？", self.window())
+        MB.yesButton.setText("继续")
+        MB.cancelButton.setText("取消")
+        if MB.exec():
+            QDesktopServices.openUrl(QUrl("https://docs.qq.com/form/page/DTW5xRXJxYWxrY1Bp"))
+        else:
+            pass
+    
+    def MoreInfoButton_Clicked(self):#更多信息按钮
+        MB = MessageBox("注意ψ(._. )>", "你正在跳转到第三方网页以进行进一步操作，是否继续？", self.window())
+        MB.yesButton.setText("继续")
+        MB.cancelButton.setText("取消")
+        if MB.exec():
+            QDesktopServices.openUrl(QUrl("https://docs.qq.com/smartsheet/DZk9WTURpdm5jSVR0"))
         else:
             pass
