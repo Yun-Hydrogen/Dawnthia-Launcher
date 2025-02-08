e0bee0a3bad7e247a3121711169266ba4cbecdb2
Yun_Hydrogen <Yun_Hydrogen@outlook.com>
2025-02-08 14:11:01 +0800

    StoryPage Finished! (USE WEBVIEW) :)

--- Main.py
+++ Main.py
@@ -1,10 +1,11 @@
 import sys,qfluentwidgets
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon,QDesktopServices
 from PyQt5.QtWidgets import QApplication
-from qfluentwidgets import FluentIcon, MSFluentWindow
-from  PyQt5.QtCore import Qt
+from qfluentwidgets import FluentIcon, MSFluentWindow, MessageBox
+from  PyQt5.QtCore import Qt,QUrl
 from FormsPage import FormsPage
 from HomePage import HomePage
+from StoryPage import StoryPage
 
 class DLLauncher(MSFluentWindow):
     def __init__(self):
@@ -13,12 +14,15 @@ class DLLauncher(MSFluentWindow):
         self.setWindowIcon(QIcon("src/ICO.png"))
         qfluentwidgets.setThemeColor("#66ccff")
         self.setFixedSize(920, 540)
+        self.setMicaEffectEnabled(False)
         
         #添加子页面
         self.HomePage = HomePage(self)
         self.addSubInterface(self.HomePage, FluentIcon.HOME, "主页")
         self.FormsPage = FormsPage(self)
         self.addSubInterface(self.FormsPage, FluentIcon.LABEL, "表单")
+        self.StoryPage = StoryPage(self)
+        self.addSubInterface(self.StoryPage, FluentIcon.BOOK_SHELF, "剧情")
 
 
 if __name__ == "__main__":
@@ -29,3 +33,5 @@ if __name__ == "__main__":
     w = DLLauncher()
     w.show()
     app.exec_()
+    
+
